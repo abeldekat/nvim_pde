d4d1f0022c2103838ce889c6c7f84e1862fcd0e2
abeldekat <abel@nomail.com>
2024-01-30 21:58:00 +0100

    sync current state to github

--- lua/ak/submodules/coding.lua
+++ lua/ak/submodules/coding.lua
@@ -2,51 +2,32 @@
 --          │            Contains plugins that modify code            │
 --          ╰─────────────────────────────────────────────────────────╯
 
--- PERFORMANCE: When later() or verylazy can be used,
--- thus loading async,
--- loading on insertenter does not improve performance.
-
--- local Util = require("ak.util")
 local Util = require("ak.util")
 local add, later = vim.cmd.packadd, Util.defer.later
 
+-- After/plugin is not loaded when using later(), which  uses vim.schedule
+-- This function is only needed when lazy-loading nvim-cmp
+local function source_after_plugin(after_path)
+  vim.cmd.source(vim.fn.stdpath("config") .. "/pack/ak_coding/opt/" .. after_path)
+end
+
 -- Lazy loading benefit: +-5 ms
--- After/plugin is not loaded when using deferring with later():
 later(function()
-  add("cmp-nvim-lsp")
-  -- after.plugin.cmp_nvim_lsp.lua
-  -- cmp_nvim_lsp must be present before nvim-lspconfig is added
-  -- does not require nvim-cmp but creates an autocmd on insertenter:
-  require("cmp_nvim_lsp").setup() -- insertenter
-end)
-later(function()
+  add("cmp-nvim-lsp") -- cmp_nvim_lsp must be present before nvim-lspconfig is added
+
   add("nvim-cmp")
   add("cmp_luasnip")
   add("cmp-buffer")
   add("cmp-path")
 end)
-later(function() -- only needed when using later()):
-  -- after.plugin.cmp_luasnip.lua:
-  require("cmp").register_source("luasnip", require("cmp_luasnip").new())
-  local cmp_luasnip = vim.api.nvim_create_augroup("cmp_luasnip", {})
-  vim.api.nvim_create_autocmd("User", {
-    pattern = "LuasnipCleanup",
-    callback = function()
-      require("cmp_luasnip").clear_cache()
-    end,
-    group = cmp_luasnip,
-  })
-  vim.api.nvim_create_autocmd("User", {
-    pattern = "LuasnipSnippetsAdded",
-    callback = function()
-      require("cmp_luasnip").refresh()
-    end,
-    group = cmp_luasnip,
-  })
-  -- after.plugin.cmp_buffer.lua:
-  require("cmp").register_source("buffer", require("cmp_buffer"))
-  -- after.plugin.cmp_path.lua:
-  require("cmp").register_source("path", require("cmp_path").new())
+later(function()
+  -- Does not require nvim-cmp but creates an autocmd on insertenter:
+  source_after_plugin("cmp-nvim-lsp/after/plugin/cmp_nvim_lsp.lua")
+
+  -- Requiring nvim-cmp:
+  source_after_plugin("cmp_luasnip/after/plugin/cmp_luasnip.lua")
+  source_after_plugin("cmp-buffer/after/plugin/cmp_buffer.lua")
+  source_after_plugin("cmp-path/after/plugin/cmp_path.lua")
 end)
 later(function() -- and finally:
   require("ak.config.completion")
@@ -74,8 +55,8 @@ later(function()
   require("ak.config.surround")
 
   -- duplicate tag in helpfile
-  -- Util.defer.on_keys(function()
-  --     add("comment-box.nvim")
-  --     require("ak.config.comment_box")
-  -- end, "<leader>bL", "Load comment-box")
+  Util.defer.on_keys(function()
+    add("comment-box.nvim")
+    require("ak.config.comment_box")
+  end, "<leader>bL", "Load comment-box")
 end)
