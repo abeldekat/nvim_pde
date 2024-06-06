796ba0741242aa0ec7e0bab43242f3d47971d83f
abeldekat <abel@nomail.com>
2024-06-06 15:20:57 +0200

    lazydev on-demand improvement

--- lua/ak/config/lang/lazydev.lua
+++ lua/ak/config/lang/lazydev.lua
@@ -19,20 +19,23 @@ local updated_workspaces = {}
 -- Restrict lazydev to one initial update per workspace
 local patch_active = true
 
-local function ws_key(ws) return ws.client_id .. ws.root end
-
 local function apply_patch()
+  local function ws_key(ws) return ws.client_id .. ws.root end
+
   local ws_update = Workspace.update
   Workspace.update = function(self)
-    local result = ws_update(self)
-    updated_workspaces[ws_key(self)] = true -- remember ws initial update
-    return result
+    updated_workspaces[ws_key(self)] = true -- remember ws passed initial update
+    return ws_update(self)
   end
 
   local ws_on_attach = Buf.on_attach
   Buf.on_attach = function(client, buf)
-    local ws = Workspace.find({ buf = buf })
-    if patch_active and updated_workspaces[ws_key(ws)] then return end
+    if patch_active then -- return early if ws has been updated
+      local ws = Workspace.find({ buf = buf })
+      if ws and updated_workspaces[ws_key(ws)] then
+        return --
+      end
+    end
     ws_on_attach(client, buf)
   end
 end
@@ -40,10 +43,9 @@ end
 local function ensure_lazydev_attached()
   local bufnr = vim.api.nvim_get_current_buf()
   local clients = vim.lsp.get_clients({ bufnr = bufnr, name = "lua_ls" })
-  if #clients ~= 1 then return end
-
-  -- lua_ls client attached to buf, make sure it's also attached to lazydev:
-  if not Buf.attached[bufnr] then Buf.on_attach(clients[1], bufnr) end
+  if clients and #clients == 1 and not Buf.attached[bufnr] then
+    Buf.on_attach(clients[1], bufnr) -- Attach to lazydev as well
+  end
 end
 
 -- Keep setting patch_active. Ensure lazydev on current buffer
