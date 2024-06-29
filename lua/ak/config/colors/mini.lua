6b892a934a0dec45e73c4eeef3eaf8f47f6d560c
abeldekat <abel@nomail.com>
2024-06-29 19:43:09 +0200

    mini.pick buffer lines current fix search result when using query modifier

--- lua/ak/config/colors/mini.lua
+++ lua/ak/config/colors/mini.lua
@@ -16,6 +16,7 @@ vim.api.nvim_create_autocmd("Colorscheme", {
   callback = function()
     local set_hl = function(name, data) vim.api.nvim_set_hl(0, name, data) end
     set_hl("MiniStatuslineModeNormal", { link = "MiniStatuslineFilename" })
+    set_hl("MiniPickMatchRanges", { fg = "orange", bold = true }) -- link='DiagnosticFloatingHint' -> { fg=p.cyan,   bg=p.bg_edge })
 
     local fg_msg_area = vim.api.nvim_get_hl(0, { name = "Comment" }).fg
     set_hl("MsgArea", { fg = fg_msg_area }) -- Area for messages and cmdline
--- lua/ak/config/editor/mini_pick.lua
+++ lua/ak/config/editor/mini_pick.lua
@@ -187,39 +187,57 @@ Pick.registry.colors = function()
   })
 end
 
+-- -- https://github.com/echasnovski/mini.nvim/discussions/988
+-- -- Fuzzy search the current buffer with syntax highlighting
+-- -- First attempt.
+-- Pick.registry.buffer_lines_current = function()
+--   local show = function(buf_id, items, query, opts)
+--     local buf_target = buf_id
+--     if items and #items > 0 then
+--       local ft = vim.bo[items[1].bufnr].filetype
+--       local has_lang, lang = pcall(vim.treesitter.language.get_lang, ft)
+--       local has_ts, _ = pcall(vim.treesitter.start, buf_target, has_lang and lang or ft)
+--       if not has_ts and ft then vim.bo[buf_target].syntax = ft end
+--     end
+--     Pick.default_show(buf_target, items, query, opts)
+--   end
+--   MiniExtra.pickers.buf_lines({ scope = "current" }, { source = { show = show } })
+-- end
+
 -- https://github.com/echasnovski/mini.nvim/discussions/988
 -- Fuzzy search the current buffer with syntax highlighting
+-- Last attempt: linenr as extmarks, but no MiniPickMatchRanges highlighting
 local ns_digit_prefix = vim.api.nvim_create_namespace("cur-buf-pick-show")
-local show_cur_buf_lines = function(buf_id, items, query, opts)
-  if items == nil or #items == 0 then return end
-
-  -- Show as usual
-  Pick.default_show(buf_id, items, query, opts)
-
-  -- Move prefix line numbers into inline extmarks
-  local lines = vim.api.nvim_buf_get_lines(buf_id, 0, -1, false)
-  local digit_prefixes = {}
-  for i, l in ipairs(lines) do
-    local _, prefix_end, prefix = l:find("^(%d+:)")
-    if prefix_end ~= nil then
-      digit_prefixes[i], lines[i] = prefix, l:sub(prefix_end + 1)
-    end
-  end
-
-  vim.api.nvim_buf_set_lines(buf_id, 0, -1, false, lines)
-  for i, pref in pairs(digit_prefixes) do
-    local extmark_opts = { virt_text = { { pref, "MiniPickNormal" } }, virt_text_pos = "inline" }
-    vim.api.nvim_buf_set_extmark(buf_id, ns_digit_prefix, i - 1, 0, extmark_opts)
-  end
-
-  -- Set highlighting based on the curent filetype
-  local ft = vim.bo[items[1].bufnr].filetype
-  local has_lang, lang = pcall(vim.treesitter.language.get_lang, ft)
-  local has_ts, _ = pcall(vim.treesitter.start, buf_id, has_lang and lang or ft)
-  if not has_ts and ft then vim.bo[buf_id].syntax = ft end
-end
-
 Pick.registry.buffer_lines_current = function()
+  local show_cur_buf_lines = function(buf_id, items, query, opts)
+    if items == nil or #items == 0 then return end
+
+    -- Show as usual
+    Pick.default_show(buf_id, items, query, opts)
+
+    -- Move prefix line numbers into inline extmarks
+    local lines = vim.api.nvim_buf_get_lines(buf_id, 0, -1, false)
+    local digit_prefixes = {}
+    for i, l in ipairs(lines) do
+      local _, prefix_end, prefix = l:find("^(%d+:)")
+      if prefix_end ~= nil then
+        digit_prefixes[i], lines[i] = prefix, l:sub(prefix_end + 1)
+      end
+    end
+
+    vim.api.nvim_buf_set_lines(buf_id, 0, -1, false, lines)
+    for i, pref in pairs(digit_prefixes) do
+      pref = string.sub(pref, 1, -2)
+      local extmark_opts = { virt_text = { { pref } }, virt_text_pos = "right_align" }
+      vim.api.nvim_buf_set_extmark(buf_id, ns_digit_prefix, i - 1, 0, extmark_opts)
+    end
+
+    -- Set highlighting based on the curent filetype
+    local ft = vim.bo[items[1].bufnr].filetype
+    local has_lang, lang = pcall(vim.treesitter.language.get_lang, ft)
+    local has_ts, _ = pcall(vim.treesitter.start, buf_id, has_lang and lang or ft)
+    if not has_ts and ft then vim.bo[buf_id].syntax = ft end
+  end
   MiniExtra.pickers.buf_lines({ scope = "current" }, { source = { show = show_cur_buf_lines } })
 end
 
