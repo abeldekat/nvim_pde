7ab3182b8bcca1a16fb5268b6eeb71bacd8a88e5
abeldekat <abel@nomail.com>
2024-06-30 20:20:52 +0200

    mini.pick pr #997: principle applied to custom oldfiles in mini.starter

--- lua/ak/config/ui/mini_starter.lua
+++ lua/ak/config/ui/mini_starter.lua
@@ -4,42 +4,21 @@ local Starter = require("mini.starter")
 local Picker = require("ak.util").pick
 local StarterOverride = {}
 
--- Only test as many items in v:oldfiles as are needed. Removed show_path
-StarterOverride.recent_files = function(n, current_dir) -- show_path
-  local function is_readable_filter(f) return vim.fn.filereadable(f) == 1 end
-  local function make_in_cwd_filter()
-    local sep = vim.loop.os_uname().sysname == "Windows_NT" and [[%\]] or "%/"
-    local cwd_pattern = "^" .. vim.pesc(vim.fn.getcwd()) .. sep
-    return function(f) return f:find(cwd_pattern) ~= nil end
-  end
+-- Optimized recent_files, only testing for filereadable on items < n
+StarterOverride.recent_files_in_cwd = function(n) -- show_path
   return function()
-    local section = string.format("Recent files%s", current_dir and " (cwd)" or "")
-    local filters = { is_readable_filter }
-    if current_dir then filters[#filters + 1] = make_in_cwd_filter() end
-
-    local files = {}
+    local section = "Recent files (cwd)"
+    local cwd = vim.fn.getcwd() .. "/"
+    local items = {}
     for _, f in ipairs(vim.v.oldfiles or {}) do
-      if #files == n then break end
-      local add = true
-      for _, filter in ipairs(filters) do
-        add = add and filter(f)
-        if not add then break end
+      if #items == n then break end
+      if vim.fn.filereadable(f) == 1 and vim.startswith(f, cwd) then
+        table.insert(items, { action = "edit " .. f, name = vim.fn.fnamemodify(f, ":t"), section = section })
       end
-      if add then files[#files + 1] = f end
     end
 
-    if #files == 0 then
-      local msg = "There are no recent files (`v:oldfiles` is empty)"
-      if current_dir then msg = "There are no recent files in current directory" end
-      return { { name = msg, action = "", section = section } }
-    end
-
-    local items = {} -- Create items
-    for _, f in ipairs(files) do
-      local name = vim.fn.fnamemodify(f, ":t") --  .. show_path(f)
-      table.insert(items, { action = "edit " .. f, name = name, section = section })
-    end
-    return items
+    local msg = "There are no recent files in current directory"
+    return #items == 0 and { { name = msg, action = "", section = section } } or items
   end
 end
 
@@ -61,7 +40,7 @@ local function make_items()
     { action = "qa", name = "Quit", section = section },
   }
   return {
-    StarterOverride.recent_files(4, true), -- starter.sections.recent_files(5, true, false),
+    StarterOverride.recent_files_in_cwd(4), -- starter.sections.recent_files(5, true, false),
     items,
   }
 end
