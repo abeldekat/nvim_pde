623ca233f1417e9270df5369addf64db6f87faa6
abeldekat <abel@nomail.com>
2024-07-06 14:21:05 +0200

    vim slime

--- lua/ak/config/util/slime.lua
+++ lua/ak/config/util/slime.lua
@@ -1,5 +1,6 @@
 vim.g.slime_target = "neovim"
 
 -- Find neovim terminal job id: echo &channel
--- After repl starts: use <c-c><c-c>
+-- Use this keymap in the terminal in normal mode
+-- The repl starts when typing <c-c><c-c>
 vim.keymap.set("n", "<leader>or", "<cmd>echom &channel<cr>", { desc = "Repl", silent = true })
