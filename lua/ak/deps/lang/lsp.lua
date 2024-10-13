3192b5a87e191186bb5bb22d787a7934b342024e
abeldekat <abel@nomail.com>
2024-10-13 10:19:22 +0200

    updates

--- lua/ak/deps/lang/lsp.lua
+++ lua/ak/deps/lang/lsp.lua
@@ -31,7 +31,7 @@ local function lsp()
     depends = {
       "folke/lazydev.nvim",
       "williamboman/mason-lspconfig.nvim",
-      { source = "mrcjkb/rustaceanvim", checkout = "5.4.2" },
+      { source = "mrcjkb/rustaceanvim", checkout = "v5.11.0" },
     },
   })
   require("ak.config.lang.diagnostics")
