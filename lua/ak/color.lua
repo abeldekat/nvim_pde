f2dc9d363c0c0045fcc8e4a341d7044214f0b08e
abeldekat <abel@nomail.com>
2025-02-23 11:12:14 +0100

    MiniNotify: Use a fidget-like window only for messages from the lsp.

--- lua/ak/color.lua
+++ lua/ak/color.lua
@@ -2,7 +2,7 @@
 -- See: colors.txt
 
 -- using sed for color and prefer_light:
-local color = "tokyonight"
+local color = "randomhue"
 local prefer_light = false
 -- end using sed
 
--- lua/ak/config/mini_notify.lua
+++ lua/ak/config/mini_notify.lua
@@ -1,53 +1,71 @@
-local Util = require("ak.util")
-local use_lsp_progress = true
+--[[
 
-local not_lua_diagnosing = function(notif) return not vim.startswith(notif.msg, "lua_ls: Diagnosing") end
-local filterout_lua_diagnosing = function(notif_arr)
-  return MiniNotify.default_sort(vim.tbl_filter(not_lua_diagnosing, notif_arr))
+Use a notification window like fidget.nvim for messages from the lsp,
+and a regular window otherwise.
+
+Do note that when the lsp emits messages, any regular message
+from vim.notify will be displayed in the same lsp window.
+This is acceptable.
+
+--]]
+
+local H = {}
+local function setup()
+  H.map("<leader>un", function() MiniNotify.clear() end, "Notify clear")
+  H.map("<leader>on", function() MiniNotify.show_history() end, "Notify history")
+  H.create_autocommmands()
+
+  require("mini.notify").setup(H.config_default)
+  vim.notify = MiniNotify.make_notify()
 end
-local format = function(notif) return notif.msg end -- skip the time
-local win_config = function()
+
+H.create_autocommmands = function()
+  local gr = vim.api.nvim_create_augroup("ak_mini_notify", {})
+  local au = function(event, pattern, callback)
+    vim.api.nvim_create_autocmd(event, { group = gr, pattern = pattern, callback = callback })
+  end
+
+  -- Remember the id of the last mininotify buffer
+  local notify_buffer
+
+  -- MiniNotify created a new buffer:
+  au("FileType", "mininotify", function(ev) notify_buffer = ev.buf end)
+
+  -- Make sure that correct buffer config is set when lsp emits messages:
+  au("LspProgress", "begin", function() vim.b.mininotify_config = H.config_on_lsp_progress end)
+
+  --  When the notify buffer is no longer displayed, make sure the buffer config is the default:
+  au("BufHidden", "*", function(ev)
+    if ev.buf == notify_buffer then vim.b.mininotify_config = nil end
+  end)
+end
+
+H.not_lua_diagnosing = function(notif) return not vim.startswith(notif.msg, "lua_ls: Diagnosing") end
+
+H.filterout_lua_diagnosing = function(notif_arr)
+  return MiniNotify.default_sort(vim.tbl_filter(H.not_lua_diagnosing, notif_arr))
+end
+
+H.format = function(notif) return notif.msg end -- skip the time and |
+
+H.win_config = function()
   local has_statusline = vim.o.laststatus > 0
-  local pad = vim.o.cmdheight + (has_statusline and 2 or 0)
+  local pad = vim.o.cmdheight + (has_statusline and 2 or 0) -- 2 instead of 1
   return { anchor = "SE", col = vim.o.columns, row = vim.o.lines - pad, border = "none" }
 end
 
-local content = {}
-if use_lsp_progress then
-  content.sort = filterout_lua_diagnosing
-  content.format = format
-end
+H.map = function(lhs, rhs, desc) vim.keymap.set("n", lhs, rhs, { desc = desc, silent = true }) end
 
-local lsp_progress = { enable = false }
-if use_lsp_progress then
-  lsp_progress.enable = true
-  lsp_progress.duration_last = 500 -- default 1000
-end
-
-local window = {}
-if use_lsp_progress then
-  window.config = win_config()
-  window.winblend = 95 -- like fidget, blend in -- 25
-  -- window.max_width_share = 0.300 -- 0.382,
-end
-
-local config = {
-  content = content,
-  lsp_progress = lsp_progress,
-  window = window,
+H.config_default = { -- lsp_progress is enabled by default
+  lsp_progress = { duration_last = 500 }, -- default 1000
+  content = { sort = H.filterout_lua_diagnosing },
 }
-require("mini.notify").setup(config)
-local notify_opts = {
-  INFO = { duration = 1000 }, -- 5000
+
+H.config_on_lsp_progress = {
+  -- Add format, removes timestamp and bar(|):
+  content = { format = H.format },
+  -- Default blend is 25. Add window like fidget.nvim:
+  window = { config = H.win_config(), winblend = 95 },
 }
-vim.notify = MiniNotify.make_notify(notify_opts)
 
-vim.keymap.set("n", "<leader>un", function() MiniNotify.clear() end, {
-  desc = "Notify clear",
-  silent = true,
-})
-
-vim.keymap.set("n", "<leader>on", function() MiniNotify.show_history() end, {
-  desc = "Notify history",
-  silent = true,
-})
+setup()
