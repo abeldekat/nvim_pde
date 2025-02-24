4674d9edfc843f0d5f860714644c66d870ff219f
abeldekat <abel@nomail.com>
2025-02-24 09:10:47 +0100

    MiniNotify: Use a fidget-like window only for messages from the lsp. Don't use vim.b.mininotify_config...

--- lua/ak/config/mini_notify.lua
+++ lua/ak/config/mini_notify.lua
@@ -15,8 +15,18 @@ local function setup()
   H.map("<leader>on", function() MiniNotify.show_history() end, "Notify history")
   H.create_autocommmands()
 
-  require("mini.notify").setup(H.config_default)
+  require("mini.notify").setup({}) -- just the defaults...
   vim.notify = MiniNotify.make_notify()
+
+  -- The idea: Based on context, use two separate configs:
+  H.config_default = MiniNotify.config -- the config constructed by mini
+  H.config_on_lsp_progress = vim.tbl_deep_extend("force", {}, H.config_default, {
+    lsp_progress = { duration_last = 500 }, -- default 1000
+    -- Add format, removes timestamp and bar(|):
+    content = { format = H.format_on_lsp_progress }, -- sort = H.filterout_lua_diagnosing
+    -- Default blend is 25. Add window like fidget.nvim:
+    window = { config = H.win_config_on_lsp_progress(), winblend = 95 },
+  })
 end
 
 H.create_autocommmands = function()
@@ -30,40 +40,31 @@ H.create_autocommmands = function()
 
   -- MiniNotify created a new buffer:
   au("FileType", "mininotify", function()
-    --  When the notify buffer is no longer displayed, make sure the buffer config falls back to default:
-    au_local("BufHidden", function() vim.b.mininotify_config = nil end)
+    --  When the notify buffer is no longer displayed, use the default config
+    au_local("BufHidden", function() MiniNotify.config = H.config_default end)
   end)
-
-  -- Make sure that correct buffer config is set when lsp emits messages:
-  au("LspProgress", "begin", function() vim.b.mininotify_config = H.config_on_lsp_progress end)
+  -- On LspProgress, use the lsp progress config
+  au("LspProgress", "begin", function() MiniNotify.config = H.config_on_lsp_progress end)
 end
 
-H.not_lua_diagnosing = function(notif) return not vim.startswith(notif.msg, "lua_ls: Diagnosing") end
-
-H.filterout_lua_diagnosing = function(notif_arr)
-  return MiniNotify.default_sort(vim.tbl_filter(H.not_lua_diagnosing, notif_arr))
+-- H.not_lua_diagnosing = function(notif) return not vim.startswith(notif.msg, "lua_ls: Diagnosing") end
+-- H.filterout_lua_diagnosing = function(notif_arr)
+--   return MiniNotify.default_sort(vim.tbl_filter(H.not_lua_diagnosing, notif_arr))
+-- end
+H.format_on_lsp_progress = function(notif) -- skip the time and the bar (|)
+  notif.hl_group = "Comment"
+  return notif.msg
 end
 
-H.format = function(notif) return notif.msg end -- skip the time and |
-
-H.win_config = function()
+H.win_config_on_lsp_progress = function()
   local has_statusline = vim.o.laststatus > 0
-  local pad = vim.o.cmdheight + (has_statusline and 2 or 0) -- 2 instead of 1
+  local pad = vim.o.cmdheight + (has_statusline and 2 or 0) -- 2 looks better than 1
   return { anchor = "SE", col = vim.o.columns, row = vim.o.lines - pad, border = "none" }
 end
 
 H.map = function(lhs, rhs, desc) vim.keymap.set("n", lhs, rhs, { desc = desc, silent = true }) end
 
-H.config_default = { -- lsp_progress is enabled by default
-  lsp_progress = { duration_last = 500 }, -- default 1000
-  content = { sort = H.filterout_lua_diagnosing },
-}
-
-H.config_on_lsp_progress = {
-  -- Add format, removes timestamp and bar(|):
-  content = { format = H.format },
-  -- Default blend is 25. Add window like fidget.nvim:
-  window = { config = H.win_config(), winblend = 95 },
-}
+H.config_default = {}
+H.config_on_lsp_progress = {}
 
 setup()
