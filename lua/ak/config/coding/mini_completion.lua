3a4cd66b53b092242a0ab4aaae5a2b3dd49c6652
abeldekat <abel@nomail.com>
2025-03-12 09:17:49 +0100

    mini.completion force mappings

--- lua/ak/config/coding/mini_completion.lua
+++ lua/ak/config/coding/mini_completion.lua
@@ -3,40 +3,22 @@
 -- ctrl-x ctrl-u: user defined completion
 
 -- Questions:
--- Can I use ctrl-j to confirm?
--- Better keymap to force completion that does not interfere with tmux
 -- Is it possible to preselect the first item?
 -- Are there situations that completion should be disabled
--- Source_func: What's the motivation to change this setting
+-- Source_func: When to change this setting
 -- How to use dadbod completion? Omnifunc? Trouble typing ctrl-x
--- When to use the template in the help to get more consistent <CR> behavior
 
--- From the help
-
--- - User can force two-stage completion via
---   |MiniCompletion.complete_twostage()| (by default is mapped to
---   `<C-Space>`) or fallback completion via
---   |MiniCompletion.complete_fallback()| (mapped to `<M-Space>`).
-
--- - LSP kind highlighting ("Function", "Keyword", etc.). Requires Neovim>=0.11.
---   By default uses "lsp" category of |MiniIcons| (if enabled). Can be customized
---   via `config.lsp_completion.process_items` by adding field <kind_hlgroup>
---   (same meaning as in |complete-items|) to items.
-
--- - Makes |mini.completion| show icons, as it uses built-in protocol map.
--- - Results in loading whole `vim.lsp` module, so might add significant amount
---   of time on startup. Call it lazily. For example, with |MiniDeps.later()|: >
-MiniIcons.tweak_lsp_kind()
+MiniIcons.tweak_lsp_kind() -- call lazily
 
 require("mini.completion").setup({
-  mappings = { -- Tmux conflict:
-    force_twostep = "", --  "<C-Space>", -- Force two-step completion
-    force_fallback = "", -- "<A-Space>", -- Force fallback completion
+  mappings = { -- Tmux conflict <C-Space>:
+    force_fallback = "<C-A-Space>", -- "<A-Space>", -- Force fallback completion
+    force_twostep = "<A-Space>", --  "<C-Space>", -- Force two-step completion
   },
 
-  lsp_completion = { -- current setup echasnovski
-    source_func = "omnifunc",
+  lsp_completion = { -- see also nvim echasnovski
     auto_setup = false,
+    source_func = "omnifunc",
     -- process_items = function(items, base)
     --   -- Don't show 'Text' and 'Snippet' suggestions
     --   items = vim.tbl_filter(function(x) return x.kind ~= 1 and x.kind ~= 15 end, items)
