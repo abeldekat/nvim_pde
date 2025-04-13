4e898e21bbfa44c06f21dd30ba9e96039e99e4f3
abeldekat <58370433+abeldekat@users.noreply.github.com>
2025-04-13 19:32:33 +0200

    Cherry picked from nvim echasnovski

--- lua/ak/config/start/misc.lua
+++ lua/ak/config/start/misc.lua
@@ -1,11 +1,7 @@
-local misc = require("mini.misc")
-misc.setup() -- misc provides a make_global option...
+local misc = require("mini.misc").setup() -- misc provides a make_global option...
 
 -- Cursor
-misc.setup_restore_cursor()
-
+MiniMisc.setup_restore_cursor()
 -- Terminal: misc.setup_termbg_sync() -- not working in tmux
 
--- Zoom
-local zoom = function() misc.zoom(0, { title = "zoom", border = "double" }) end
-vim.keymap.set("n", "<leader>uz", zoom, { desc = "Toggle zoom buffer", silent = true })
+vim.keymap.set("n", "<leader>uz", "<cmd>lua MiniMisc.zoom()<cr>", { desc = "Toggle zoom buffer", silent = true })
--- lua/ak/deps/start.lua
+++ lua/ak/deps/start.lua
@@ -5,7 +5,8 @@ local now, later = MiniDeps.now, MiniDeps.later
 local function icons() require("ak.config.start.icons") end -- cmp, explorer, etc...
 
 now(function()
-  require("ak.config.options")
+  require("ak.config.start.options")
+  require("ak.config.start.autocmds")
   require("ak.config.start.misc") -- ie last cursor position autocmd
 end)
 
@@ -16,7 +17,8 @@ else
 end
 
 later(function()
-  require("ak.config.keymaps")
+  require("ak.config.start.keymaps")
+  require("ak.config.start.diagnostics")
   require("ak.config.start.basics") -- not using options and autocmd, so load later...
   require("ak.config.start.notify") -- better installation process
   require("ak.config.start.extra") -- pickers and ai. Pickers are not registered.
