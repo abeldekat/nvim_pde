8c08646791b93baeabc73861b2929c7ff130b22b
abeldekat <58370433+abeldekat@users.noreply.github.com>
2025-05-28 23:20:14 +0200

    Correct configuration for main branch of nvim-treesitter-textobjects. Also remove its repeatable_move, and the class mappings.

--- lua/ak/config/coding/textobjects_treesitter.lua
+++ lua/ak/config/coding/textobjects_treesitter.lua
@@ -1,111 +1,83 @@
---          ╭─────────────────────────────────────────────────────────╮
---          │treesitter textobjects: uses k for block, b is preserved │
---          ╰─────────────────────────────────────────────────────────╯
+-- Textobjects: uses k for block, b is preserved.
+-- Not using repeatable_move.
+-- Not using class functionality.
+-- TODO: Is move useful in normal mode, when using jump2d?
 
-local Util = require("ak.util")
-local repeatable_move_enabled = false -- toggle
+local use_mini_ai = require("ak.util").use_mini_ai
 
-local function get_opts()
-  local opts = {
-    move = {
-      enable = true,
-      set_jumps = true,
-      goto_next_start = {
-        ["]c"] = { query = "@class.outer", desc = "Next class start" },
-        ["]k"] = { query = "@block.outer", desc = "Next block start" },
-        ["]f"] = { query = "@function.outer", desc = "Next function start" },
-        ["]a"] = { query = "@parameter.inner", desc = "Next argument start" },
-      },
-      goto_next_end = {
-        ["]C"] = { query = "@class.outer", desc = "Next class end" },
-        ["]K"] = { query = "@block.outer", desc = "Next block end" },
-        ["]F"] = { query = "@function.outer", desc = "Next function end" },
-        ["]A"] = { query = "@parameter.inner", desc = "Next argument end" },
-      },
-      goto_previous_start = {
-        ["[c"] = { query = "@class.outer", desc = "Previous class start" },
-        ["[k"] = { query = "@block.outer", desc = "Previous block start" },
-        ["[f"] = { query = "@function.outer", desc = "Previous function start" },
-        ["[a"] = { query = "@parameter.inner", desc = "Previous argument start" },
-      },
-      goto_previous_end = {
-        ["[C"] = { query = "@class.outer", desc = "Previous class end" },
-        ["[K"] = { query = "@block.outer", desc = "Previous block end" },
-        ["[F"] = { query = "@function.outer", desc = "Previous function end" },
-        ["[A"] = { query = "@parameter.inner", desc = "Previous argument end" },
-      },
-    },
-    swap = {
-      enable = true,
-      swap_next = {
-        [">K"] = { query = "@block.outer", desc = "Swap next block" },
-        [">F"] = { query = "@function.outer", desc = "Swap next function" },
-        [">A"] = { query = "@parameter.inner", desc = "Swap next argument" },
-      },
-      swap_previous = {
-        ["<K"] = { query = "@block.outer", desc = "Swap previous block" },
-        ["<F"] = { query = "@function.outer", desc = "Swap previous function" },
-        ["<A"] = { query = "@parameter.inner", desc = "Swap previous argument" },
-      },
-    },
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = { -- NOTE: Don't use 'l', interferes with mini.ai last motion
-        ["a="] = { query = "@assignment.outer", desc = "outer part of assignment" },
-        ["i="] = { query = "@assignment.inner", desc = "inner part of assignment" },
-        -- ["l="] = { query = "@assignment.lhs", desc = "left hand side of assignment" },
-        ["k="] = { query = "@assignment.lhs", desc = "left hand side of assignment" },
-        -- ["r="] = { query = "@assignment.rhs", desc = "right hand side of assignment" },
-        ["v="] = { query = "@assignment.rhs", desc = "right hand side of assignment" },
-        -- block
-        ["ak"] = { query = "@block.outer", desc = "around block" },
-        ["ik"] = { query = "@block.inner", desc = "inside block" },
-        -- class
-        ["ac"] = { query = "@class.outer", desc = "around class" },
-        ["ic"] = { query = "@class.inner", desc = "inside class" },
-        -- disabled mini.ai ? interactive. mini.indentscope has ii and ai
-        ["a?"] = { query = "@conditional.outer", desc = "around conditional" },
-        ["i?"] = { query = "@conditional.inner", desc = "inside conditional" },
-        -- loop:
-        ["ao"] = { query = "@loop.outer", desc = "around loop" },
-        ["io"] = { query = "@loop.inner", desc = "inside loop" },
-      },
-    },
-  }
-  if not Util.use_mini_ai then
-    opts.select.keymaps["af"] = { query = "@function.outer", desc = "around function" }
-    opts.select.keymaps["if"] = { query = "@function.inner", desc = "inside function" }
-    opts.select.keymaps["aa"] = { query = "@parameter.outer", desc = "around argument" }
-    opts.select.keymaps["ia"] = { query = "@parameter.inner", desc = "inside argument" }
-  end
-  return opts
+require("nvim-treesitter-textobjects").setup({ select = { lookahead = true } })
+
+-- Define your own text objects mappings similar to ip (inner paragraph) and ap (a paragraph).
+local sel = require("nvim-treesitter-textobjects.select").select_textobject
+local mapsel = function(lhs, query, desc, from_scm)
+  desc = desc and "Select " .. desc or ""
+  from_scm = from_scm or "textobjects"
+  vim.keymap.set({ "x", "o" }, lhs, function() sel(query, from_scm) end, { desc = desc, silent = true })
 end
 
-local function toggle_repeatable_move()
-  local modes = { "n", "x", "o" }
+mapsel("a=", "@assignment.outer", "around assignment")
+mapsel("i=", "@assignment.inner", "inside assignment")
+mapsel("k=", "@assignment.lhs", "lhs of assignment")
+mapsel("v=", "@assignment.rhs", "rhs of assignment")
+mapsel("ak", "@block.outer", "around block")
+mapsel("ik", "@block.inner", "inside block")
+-- mapsel("ac", "@class.outer", "around class")
+-- mapsel("ic", "@class.inner", "inside class")
+mapsel("ao", "@loop.outer", "around loop")
+mapsel("io", "@loop.inner", "inside loop")
 
-  local function enable()
-    local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-    vim.keymap.set(modes, ";", ts_repeat_move.repeat_last_move)
-    vim.keymap.set(modes, ",", ts_repeat_move.repeat_last_move_opposite)
-    vim.keymap.set(modes, "f", ts_repeat_move.builtin_f_expr, { expr = true })
-    vim.keymap.set(modes, "F", ts_repeat_move.builtin_F_expr, { expr = true })
-    vim.keymap.set(modes, "t", ts_repeat_move.builtin_t_expr, { expr = true })
-    vim.keymap.set(modes, "T", ts_repeat_move.builtin_T_expr, { expr = true })
-  end
-  local function disable()
-    for _, key in ipairs({ ";", ",", "f", "F", "t", "T" }) do
-      pcall(vim.keymap.del, modes, key)
-    end
-  end
-
-  repeatable_move_enabled = not repeatable_move_enabled
-  -- stylua: ignore
-  if repeatable_move_enabled then enable() else disable() end
+-- Use a? and i? instead of mini.ai interactive. mini.indentscope has ii and ai:
+mapsel("a?", "@conditional.outer", "around conditional")
+mapsel("i?", "@conditional.inner", "inside conditional")
+if not use_mini_ai then -- NOTE: Don't use 'l', interferes with mini.ai last motion
+  mapsel("af", "@function.outer", "around function")
+  mapsel("if", "@function.inner", "inside function")
+  mapsel("aa", "@parameter.outer", "around argument")
+  mapsel("ia", "@parameter.inner", "inside argument")
 end
 
-vim.keymap.set("n", "<leader>uM", toggle_repeatable_move, { desc = "Toggle treesitter repeatable move" })
+-- Define your own mappings to swap the node under the cursor with the next or previous one,
+-- like function parameters or arguments.
+local swap = require("nvim-treesitter-textobjects.swap")
+local swap_next = swap.swap_next
+local swap_prev = swap.swap_prev
+local mapswap = function(lhs, rhs_function, query, desc)
+  desc = desc and "Swap " .. desc or ""
+  vim.keymap.set("n", lhs, function() rhs_function(query) end, { desc = desc, silent = true })
+end
 
--- In diff mode, use the default vim text objects c & C instead of the treesitter ones.
-if not vim.wo.diff then require("nvim-treesitter-textobjects").setup(get_opts()) end
+mapswap(">K", swap_next, "@block.outer", "next block")
+mapswap(">F", swap_next, "@function.outer", "next function")
+mapswap(">A", swap_next, "@parameter.inner", "next argument")
+mapswap("<K", swap_prev, "@block.outer", "previous block")
+mapswap("<F", swap_prev, "@function.outer", "previous function")
+mapswap("<A", swap_prev, "@parameter.inner", "previous argument")
+
+-- Define your own mappings to jump to the next or previous text object.
+-- This is similar to ]m, [m, ]M, [M Neovim's mappings to jump to the next or previous function.
+local move = require("nvim-treesitter-textobjects.move")
+local move_next_start = move.goto_next_start
+local move_next_end = move.goto_next_end
+local move_prev_start = move.goto_prev_start
+local move_prev_end = move.goto_prev_end
+local mapmove = function(lhs, rhs_function, query, desc, from_scm)
+  from_scm = from_scm or "textobjects"
+  vim.keymap.set({ "n", "x", "o" }, lhs, function() rhs_function(query, from_scm) end, { desc = desc, silent = true })
+end
+
+-- mapmove("]c", move_next_start, "@class.outer", "Next class start")
+mapmove("]k", move_next_start, "@block.outer", "Next block start")
+mapmove("]f", move_next_start, "@function.outer", "Next function start")
+mapmove("]a", move_next_start, "@parameter.inner", "Next argument start")
+-- mapmove("]C", move_next_end, "@class.outer", "Next class end")
+mapmove("]K", move_next_end, "@block.outer", "Next block end")
+mapmove("]F", move_next_end, "@function.outer", "Next function end")
+mapmove("]A", move_next_end, "@parameter.inner", "Next argument end")
+-- mapmove("[c", move_prev_start, "@class.outer", "Previous class start")
+mapmove("[k", move_prev_start, "@block.outer", "Previous block start")
+mapmove("[f", move_prev_start, "@function.outer", "Previous function start")
+mapmove("[a", move_prev_start, "@parameter.inner", "Previous argument start")
+-- mapmove("[C", move_prev_end, "@class.outer", "Previous class end")
+mapmove("[K", move_prev_end, "@block.outer", "Previous block end")
+mapmove("[F", move_prev_end, "@function.outer", "Previous function end")
+mapmove("[A", move_prev_end, "@parameter.inner", "Previous argument end")
