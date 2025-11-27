ba3d215d62596622d02ce5f115c931a3235322ce
abeldekat <58370433+abeldekat@users.noreply.github.com>
2025-11-27 19:44:12 +0100

    extra mini.ai

--- lua/ak/mini/ai.lua
+++ lua/ak/mini/ai.lua
@@ -1,13 +1,27 @@
-local ai = require("mini.ai")
+local ai = require('mini.ai')
+local e = MiniExtra
+
 ai.setup({
   -- 'mini.ai' can be extended with custom textobjects
   custom_textobjects = {
     -- Make `aB` / `iB` act on around/inside whole *b*uffer
-    B = MiniExtra.gen_ai_spec.buffer(),
+    B = e.gen_ai_spec.buffer(),
+
     -- For more complicated textobjects that require structural awareness,
     -- use tree-sitter. This example makes `aF`/`iF` mean around/inside function
     -- definition (not call). See `:h MiniAi.gen_spec.treesitter()` for details.
-    F = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
+    D = e.gen_ai_spec.diagnostic(),
+
+    F = ai.gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }),
+
+    I = e.gen_ai_spec.indent(),
+    L = e.gen_ai_spec.line(),
+    N = e.gen_ai_spec.number(),
+
+    o = ai.gen_spec.treesitter({
+      a = { '@block.outer', '@loop.outer', '@conditional.outer' },
+      i = { '@block.inner', '@loop.inner', '@conditional.inner' },
+    }),
   },
 
   -- 'mini.ai' by default mostly mimics built-in search behavior: first try
@@ -16,5 +30,5 @@ ai.setup({
   -- always try to search only covering textobject and explicitly ask to search
   -- for next (`an`/`in`) or last (`al`/`il`).
   -- Try this. If you don't like it - delete next line and this comment.
-  search_method = "cover",
+  search_method = 'cover',
 })
