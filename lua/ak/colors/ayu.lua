8dd310be1429c320bcf3a793182fa6d29672fa22
abeldekat <58370433+abeldekat@users.noreply.github.com>
2025-11-28 09:37:02 +0100

    copied ayu modifications from nvim @pkazmier

--- lua/ak/colors/ayu.lua
+++ lua/ak/colors/ayu.lua
@@ -9,13 +9,66 @@ _G.Config.add_theme_info('ayu*', info, 'Ayu variants')
 
 require('ayu').setup({
   mirage = true,
-  -- overrides = function()
-  --   local c = require("ayu.colors")
-  --   return {
-  --     MiniHipatternsFixme = { fg = c.bg, bg = c.error, bold = true },
-  --     MiniHipatternsHack = { fg = c.bg, bg = c.keyword, bold = true },
-  --     MiniHipatternsTodo = { fg = c.bg, bg = c.tag, bold = true },
-  --     MiniHipatternsNote = { fg = c.bg, bg = c.regexp, bold = true },
-  --   }
-  -- end,
+  terminal = true,
+  overrides = function() -- code fragments copied from nvim @pkazmier, 00_ayu.lua
+    local c = require('ayu.colors')
+      -- stylua: ignore
+    return {
+      FloatTitle                     = { bold = true },
+      LineNr                         = { fg = c.guide_active },
+      CursorLineNr                   = { fg = c.accent, bg = c.bg, bold = true },
+
+      -- Now that 'pumborder' exists, use same styling for other floats
+      Pmenu                          = { fg = c.fg, bg = c.bg },
+      PmenuMatch                     = { fg = c.regexp },
+      PmenuSel                       = { bg = c.line, reverse = false, bold = true },
+
+      -- Use reverse text for diagnostics
+      DiagnosticVirtualTextError     = { bg = c.error,   fg = c.line, italic = true },
+      DiagnosticVirtualTextWarn      = { bg = c.keyword, fg = c.line, italic = true },
+      DiagnosticVirtualTextInfo      = { bg = c.tag,     fg = c.line, italic = true },
+      DiagnosticVirtualTextHint      = { bg = c.regexp,  fg = c.line, italic = true },
+
+      -- Bold current line in MiniFiles
+      MiniFilesCursorLine            = { fg = nil,        bg = c.line,   bold = true },
+      MiniFilesTitleFocused          = { fg = c.panel_bg, bg = c.regexp, bold = true },
+
+      -- Bold matches and current line in MiniPick
+      MiniPickMatchCurrent           = { fg = nil, bg = c.line,          bold = true },
+      MiniPickMatchMarked            = { fg = nil, bg = c.gutter_normal, bold = true },
+
+      -- Dim inactive MiniStarter elements
+      MiniStarterInactive            = { link = "MiniJump2dDim" },
+      MiniStarterSection             = { fg = c.keyword, bold = true },
+      MiniStarterHeader              = { fg = c.tag,     bold = true },
+
+      MiniStatuslineFilename         = { fg = c.fg,             bg = c.panel_border,  bold = true },
+      MiniStatuslineDevinfo          = { fg = c.fg,             bg = c.selection_bg },
+      MiniStatuslineFileinfo         = { fg = c.fg,             bg = c.selection_bg },
+      StatusLine                     = { fg = c.fg,             bg = c.panel_border },
+      StatusLineNC                   = { fg = c.fg,             bg = c.panel_border },
+
+      -- Extend the context highlighting to line numbers as well
+      TreesitterContext              = { bg = c.selection_inactive },
+      TreesitterContextBottom        = { sp = c.panel_bg,     underline = true },
+      TreesitterContextLineNumber    = { fg = c.guide_active, bg = c.selection_inactive },
+
+      ['@markup.heading']            = { fg = c.keyword,  bold = true },
+      ['@markup.heading.1']          = { fg = c.accent,   bold = true },
+      ['@markup.heading.2']          = { fg = c.keyword,  bold = true },
+      ['@markup.heading.3']          = { fg = c.markup,   bold = true },
+      ['@markup.heading.4']          = { fg = c.entity,   bold = true },
+      ['@markup.heading.5']          = { fg = c.regexp,   bold = true },
+      ['@markup.heading.6']          = { fg = c.string,   bold = true },
+      ['@markup.strong']             = { fg = c.keyword,  bold = true },
+      ['@markup.italic']             = { fg = c.keyword,  italic = true },
+      ['@markup.quote']              = { fg = c.constant, italic = true },
+      ['@markup.raw']                = { fg = c.tag,      bg = c.selection_inactive },
+      ['@markup.list']               = { fg = c.vcs_added },
+      ['@markup.raw.block']          = { fg = c.tag },
+      ['@module']                    = { fg = c.fg },
+      ['@string.documentation']      = { fg = c.lsp_inlay_hint },
+      ['@variable.builtin']          = { fg = c.fg },
+    }
+  end,
 })
