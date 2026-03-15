febbcb9d84d01b94cf80acebce6e2c956f0ceaf5
abeldekat <58370433+abeldekat@users.noreply.github.com>
2026-03-15 13:55:31 +0100

    remove local .luarc.jsonc in favor of lua_ls MiniMax style

--- after/lsp/lua_ls.lua
+++ after/lsp/lua_ls.lua
@@ -1,15 +1,3 @@
--- assumption: .luarc.jsonc takes precendence. individual values override,
--- arrays are not merged
--- example: diagnostics global array, "one" in lua_ls settings,
--- and "two" in .luarc.json -> only a warning on global "one"
--- note: when uncommenting a library block in .luarc.jsonc, the completion
--- becomes available without restarting nvim
-
--- luals "go to source" =======================================================
--- deal with the fact that luals in case of `local a = function()` style
--- treats both `a` and `function()` as definitions of `a`.
--- do this by tweaking `vim.lsp.buf_definition` mapping as client-local
--- handlers are ignored after https://github.com/neovim/neovim/pull/30877
 local filter_line_locations = function(locations)
   local present, res = {}, {}
   for _, l in ipairs(locations) do
@@ -28,41 +16,43 @@ local show_location = function(location)
   vim.bo[buf_id].buflisted = true
   vim.api.nvim_win_set_buf(0, buf_id)
   vim.api.nvim_win_set_cursor(0, { location.lnum, location.col - 1 })
-  vim.cmd("normal! zv")
+  vim.cmd('normal! zv')
 end
 
 local on_list = function(args)
   local items = filter_line_locations(args.items)
   if #items > 1 then
-    vim.fn.setqflist({}, " ", { title = "lsp locations", items = items })
-    return vim.cmd("botright copen")
+    vim.fn.setqflist({}, ' ', { title = 'lsp locations', items = items })
+    return vim.cmd('botright copen')
   end
   show_location(items[1])
 end
 
+-- Deal with the fact that LuaLS in case of `local a = function()` style
+-- treats both `a` and `function()` as definitions of `a`.
 local luals_unique_definition = function()
-  -- using ctrl-o and ctrl-i from jumplist:
-  vim.cmd("normal! m'") -- see mini.nvim issue 979, and ak.config.editor.mini_pick.lua
+  vim.cmd("normal! m'") -- using ctrl-o and ctrl-i from jumplist, see mini.nvim issue 979
 
   return vim.lsp.buf.definition({ on_list = on_list })
 end
 
 return {
-  on_attach = function(client, bufnr)
-    -- reduce unnecessarily long list of completion triggers for better 'mini.completion' experience
-    -- TODO: reduce more see MiniMax
-    client.server_capabilities.completionProvider.triggerCharacters = { ".", ":" }
-    vim.keymap.set("n", "gd", luals_unique_definition, { buffer = bufnr, desc = "goto definition" })
+  on_attach = function(client, buf_id)
+    -- Reduce very long list of triggers for better 'mini.completion' experience
+    client.server_capabilities.completionProvider.triggerCharacters = { '.', ':', '#', '(' }
+
+    -- Override global "Go to source" mapping with dedicated buffer-local
+    vim.keymap.set('n', 'gd', luals_unique_definition, { buffer = buf_id, desc = 'goto definition' })
   end,
   settings = {
-    lua = {
-      runtime = { version = "luajit", path = vim.split(package.path, ";") },
+    Lua = {
+      runtime = { version = 'LuaJIT', path = vim.split(package.path, ';') },
       diagnostics = {
-        -- get the language server to recognize common globals
-        globals = { "vim", "describe", "it", "before_each", "after_each" },
-        -- disable = { "need-check-nil" },
-        -- don't make workspace diagnostic, as it consumes too much cpu and ram
-        workspacedelay = -1,
+        workspacedelay = -1, -- don't make workspace diagnostic, as it consumes too much cpu and ram
+      },
+      workspace = {
+        ignoreSubmodules = true,
+        library = { vim.env.VIMRUNTIME },
       },
       telemetry = { enable = false },
     },
