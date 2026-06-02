6f2f14e7c4cb4ca027d43d461f0df2b2a8a8cae4
abeldekat <58370433+abeldekat@users.noreply.github.com>
2026-06-02 16:41:17 +0200

    MiniMax: move Config.on_packchanged definition before first vim.pack.add

--- init.lua
+++ init.lua
@@ -1,31 +1,16 @@
 ---@diagnostic disable: duplicate-set-field
 
--- Added: Optimize( a bit )
-for _, disable in ipairs({ 'gzip', 'tarPlugin', 'tutor', 'zipPlugin' }) do
-  vim.g['loaded_' .. disable] = 0
-end
-
--- Install 'mini.nvim'
-vim.pack.add({ 'https://github.com/nvim-mini/mini.nvim' })
-
--- Define config table to be able to pass data between scripts
+-- Define config table to be able to pass data between scripts.
 _G.Config = {}
 
--- Loading helpers used to organize config into fail-safe parts
-local misc = require('mini.misc')
-Config.now = function(f) misc.safely('now', f) end
-Config.later = function(f) misc.safely('later', f) end
-Config.now_if_args = vim.fn.argc(-1) > 0 and Config.now or Config.later
-Config.on_event = function(ev, f) misc.safely('event:' .. ev, f) end
-Config.on_filetype = function(ft, f) misc.safely('filetype:' .. ft, f) end
-
--- Define custom autocommand group and helper to create an autocommand
+-- Define custom autocommand group and helper to create an autocommand.
 local gr = vim.api.nvim_create_augroup('custom-config', {})
 Config.new_autocmd = function(event, pattern, callback, desc)
   local opts = { group = gr, pattern = pattern, callback = callback, desc = desc }
   vim.api.nvim_create_autocmd(event, opts)
 end
 
+-- Define custom `vim.pack.add()` hook helper.
 Config.on_packchanged = function(plugin_name, kinds, callback, desc)
   local f = function(ev)
     local name, kind = ev.data.spec.name, ev.data.kind
@@ -35,3 +20,19 @@ Config.on_packchanged = function(plugin_name, kinds, callback, desc)
   end
   Config.new_autocmd('PackChanged', '*', f, desc)
 end
+
+-- Added: Optimize( a bit )..
+for _, disable in ipairs({ 'gzip', 'tarPlugin', 'tutor', 'zipPlugin' }) do
+  vim.g['loaded_' .. disable] = 0
+end
+
+-- Load 'mini.nvim'.
+vim.pack.add({ 'https://github.com/nvim-mini/mini.nvim' })
+
+-- Loading helpers used to organize config into fail-safe parts.
+local misc = require('mini.misc')
+Config.now = function(f) misc.safely('now', f) end
+Config.later = function(f) misc.safely('later', f) end
+Config.now_if_args = vim.fn.argc(-1) > 0 and Config.now or Config.later
+Config.on_event = function(ev, f) misc.safely('event:' .. ev, f) end
+Config.on_filetype = function(ft, f) misc.safely('filetype:' .. ft, f) end
