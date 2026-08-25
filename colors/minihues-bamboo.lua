178ce5da039ff02f7d05f97b21538611e26cb8fb
abeldekat <58370433+abeldekat@users.noreply.github.com>
2026-08-25 20:15:43 +0200

    minihues-bamboo

--- colors/minihues-bamboo.lua
+++ colors/minihues-bamboo.lua
@@ -1,10 +1,35 @@
--- Bamboo vulgaris bg1 and fg
-local hues = require('mini.hues')
-hues.setup({
-  background = '#252623',
-  foreground = '#f1e9d2',
-  n_hues = 8,
-  saturation = 'medium',
-  accent = 'bg',
-})
+-- Generate hues for bamboo vulgaris using its bg0 and fg, with default MiniHue setup
+--
+-- local opts = { background = '#252623', foreground = '#f1e9d2' }
+-- require('mini.hues').setup(opts)
+
+-- stylua: ignore
+local generated = {
+  bg = '#252623', bg_edge = '#181916', bg_edge2 = '#090a08', bg_mid = '#424440', bg_mid2 = '#62635f',
+  fg = '#f1e9d2', fg_edge = '#f8f0d9', fg_edge2 = '#fff7e0', fg_mid = '#cec6b0', fg_mid2 = '#aaa38d',
+  accent = '#e0f2b7', accent_bg = '#252623',
+
+  azure = '#b6e1ff', azure_bg = '#003351',
+  blue = '#d4d2ff', blue_bg = '#241d4a',
+  cyan = '#a8fbfb', cyan_bg = '#004e4f',
+  green = '#c2f9cf', green_bg = '#003515',
+  orange = '#ffd7b3', orange_bg = '#4a2700',
+  purple = '#ffd4fe', purple_bg = '#3b1437',
+  red = '#ffcbcd', red_bg = '#451017',
+  yellow = '#efedaf', yellow_bg = '#3c3900',
+}
+
+-- stylua: ignore
+local palette_vulgaris = {
+  -- azure is not present. Bamboo does define grey and coral
+  blue = '#57a5e5',blue_bg = '#68aee8',
+  cyan = '#70c2be',
+  green = '#8fb573',
+  orange = '#ff9966',
+  purple = '#df73ff', -- bamboo bright_purple, purple is too much like blue
+  red = '#e75a7c',
+  yellow = '#dbb651', yellow_bg = '#e2c792',
+}
+
+require('mini.hues').apply_palette(vim.tbl_deep_extend('force', generated, palette_vulgaris))
 vim.g.colors_name = 'minihues-bamboo'
