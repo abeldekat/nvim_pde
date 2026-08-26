c2dc179d46abd374de3383fc4e5aba4dd091f43e
abeldekat <58370433+abeldekat@users.noreply.github.com>
2026-08-26 14:27:53 +0200

    colors: thorn, restrict number of plugins

--- lua/ak/colors/thorn.lua
+++ lua/ak/colors/thorn.lua
@@ -1,8 +1,10 @@
-local prefer_light = require('ak.color').prefer_light
-vim.o.background = prefer_light and 'light' or 'dark'
-local name = 'thorn'
+vim.o.background = require('ak.color').prefer_light and 'light' or 'dark'
 
+local name = 'thorn'
 Config.add_theme_info(name, { name = name, variants = { name } }, 'Thorn dummy variants')
+
+-- Restrict the number of supported plugins
+require('thorn.groups').plugins = { mini = true }
 require('thorn').setup({
   styles = { diagnostic = { error = { highlight = false } } },
 })
