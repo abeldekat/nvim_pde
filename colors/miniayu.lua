0db14bd26dc3f8b2bd7321bee5714bfa23303c99
abeldekat <58370433+abeldekat@users.noreply.github.com>
2026-09-09 14:27:49 +0200

    colors: minor refactoring

--- colors/miniayu.lua
+++ colors/miniayu.lua
@@ -5,14 +5,22 @@
 
 -- stylua: ignore
 local generated = {
-  bg = '#1F2430', bg_edge = '#131723', bg_edge2 = '#050913', bg_mid = '#383d4a', bg_mid2 = '#525866',
-  fg = '#CCCAC2', fg_edge = '#dddbd3', fg_edge2 = '#efede5', fg_mid = '#aeaca5', fg_mid2 = '#918f87',
-  accent_bg = '#1F2430',
-  azure_bg = '#004c51', blue_bg = '#002a4c', cyan_bg = '#003c23', green_bg = '#343700',
-  orange_bg = '#430f10', purple_bg = '#271a47', red_bg = '#3c1131', yellow_bg = '#4a2b00'
+  bg = "#1F2430", bg_edge = "#131723", bg_edge2 = "#050913", bg_mid = "#383d4a", bg_mid2 = "#525866",
+  fg = "#CCCAC2", fg_edge = "#dddbd3", fg_edge2 = "#efede5", fg_mid = "#aeaca5", fg_mid2 = "#918f87",
+  accent = "#b3c9ff", accent_bg = "#1F2430",
+
+  azure = "#89dae2", azure_bg = "#004c51",
+  blue = "#a1cffc", blue_bg = "#002a4c",
+  cyan = "#9ddab7", cyan_bg = "#003c23",
+  green = "#c9d094", green_bg = "#343700",
+  orange = "#fab6b1", orange_bg = "#430f10",
+  purple = "#ccc1fa", purple_bg = "#271a47",
+  red = "#eeb6dc", red_bg = "#3c1131",
+  yellow = "#edc191", yellow_bg = "#4a2b00"
 }
+
 -- stylua: ignore
-local fg = {
+local palette = {
   accent = '#FFCC66', -- colors.accent
 
   -- Using colors set for MiniIcons*...
@@ -22,5 +30,5 @@ local fg = {
   orange = '#FFAD66', purple = '#D3B8F9', red = '#FF6666', yellow = '#FFDFB3',
 }
 
-require('mini.hues').apply_palette(vim.tbl_deep_extend('force', generated, fg))
+require('mini.hues').apply_palette(vim.tbl_deep_extend('force', generated, palette))
 vim.g.colors_name = 'miniayu'
--- colors/minimelange.lua
+++ colors/minimelange.lua
@@ -4,31 +4,37 @@
 -- local opts = { background = '#292522', foreground = '#ECE1D7' }
 -- require('mini.hues').setup(opts)
 
--- stylua: ignore start
+-- stylua: ignore
 local generated = {
-  bg = '#292522', bg_edge = '#1c1815', bg_edge2 = '#0c0907', bg_mid = '#46423e', bg_mid2 = '#65605c',
-  fg = '#ECE1D7', fg_edge = '#f5eae0', fg_edge2 = '#fff4ea', fg_mid = '#cabfb5', fg_mid2 = '#a89d94',
-  accent = '#ffd3af', accent_bg = '#292522',
-  azure = "#aaebff", azure_bg = '#004655',
+  bg = "#292522", bg_edge = "#1c1815", bg_edge2 = "#0c0907", bg_mid = "#46423e", bg_mid2 = "#65605c",
+  fg = "#ECE1D7", fg_edge = "#f5eae0", fg_edge2 = "#fff4ea", fg_mid = "#cabfb5", fg_mid2 = "#a89d94",
+  accent = "#ffd3af", accent_bg = "#292522",
+
+  azure = "#aaebff", azure_bg = "#004655",
+  blue = "#bed5ff", blue_bg = "#12234c",
+  cyan = "#aaf5e0", cyan_bg = "#00483b",
+  green = "#d2eeb6", green_bg = "#1e3200",
+  orange = "#ffccb8", orange_bg = "#441402",
   purple = "#edd0ff", purple_bg = "#321843",
+  red = "#ffcbe2", red_bg = "#421128",
+  yellow = "#fce0a6", yellow_bg = "#493400"
 }
 
--- Melange palette defines sections "a"(grays), "b"(fg bright), "c"(fg) and "d"(bg)
--- Melange does not define azure, orange and purple. It does define magenta
-local b_fg = { blue = '#A3A9CE', cyan = '#89B3B6', green = '#85B695', red = '#D47766', yellow = '#EBC06D' }
--- local c = { blue = '#7F91B2', cyan = '#7B9695', green = '#78997A', red = '#BD8183', yellow = '#E49B5D' }
-local d_bg = {
-  blue_bg = '#273142', cyan_bg = '#253333', green_bg = '#233524', red_bg = '#7D2A2F', yellow_bg = '#8B7449'
+-- Melange's palette has sections "a"(grays), "b"(fg bright), "c"(fg) and "d"(bg)
+-- No azure, orange and purple colors. MiniHues does not use magenta
+-- stylua: ignore
+local palette = { -- b and d sections
+  blue = '#A3A9CE', blue_bg = '#273142',
+  cyan = '#89B3B6', cyan_bg = '#253333',
+  -- Melange green is not bright enough for mini.hues. Use generated
+  -- green = '#85B695', green_bg = '#233524',
+  red = '#D47766', red_bg = '#7D2A2F',
+  yellow = '#EBC06D', yellow_bg = '#8B7449',
 }
--- stylua: ignore end
 
--- Use c.yellow for orange
-b_fg.orange = '#E49B5D'
-d_bg.orange_bg = d_bg.yellow_bg
+-- Use yellow from c section
+palette.orange = '#E49B5D'
+palette.orange_bg = palette.yellow_bg
 
--- Melange green is not bright enough for mini.hues. Use generated
-b_fg.green = '#d2eeb6'
-d_bg.green_bg = '#1e3200'
-
-require('mini.hues').apply_palette(vim.tbl_deep_extend('force', generated, d_bg, b_fg))
+require('mini.hues').apply_palette(vim.tbl_deep_extend('force', generated, palette))
 vim.g.colors_name = 'minimelange'
